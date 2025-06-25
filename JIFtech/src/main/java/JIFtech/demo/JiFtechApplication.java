46aca6df3fd8454615af9f1a2af846f299a549e0
Jose S.C <Conejiz1929@gmail.com>
2025-06-24 20:51:36 -0600

    Inicio
    Inicio proyecto

---
+++ JIFtech/src/main/java/JIFtech/demo/JiFtechApplication.java
@@ -0,0 +1,13 @@
+package JIFtech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiFtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiFtechApplication.class, args);
+	}
+
+}
---
+++ JIFtech/src/test/java/JIFtech/demo/JiFtechApplicationTests.java
@@ -0,0 +1,13 @@
+package JIFtech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiFtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
