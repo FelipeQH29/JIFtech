dde9255037527ae666e4f18c9228dccbddf90ea5
IanBrenes <iaguilar60911@ufide.ac.cr>
2025-06-27 00:03:41 -0600

    commit index

---
+++ TiendaJIF/src/test/java/TiendaJIF/demo/TiendaJifApplicationTests.java
@@ -0,0 +1,13 @@
+package TiendaJIF.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
